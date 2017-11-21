5372ab25b6ff913cc196082b3b09173bd1580708
SarahYuHanCheng <bigbigtreeful@gmail.com>
2017-11-22 04:32:54 +0800

    Only turn with msg back from server in  update

--- car.js
+++ car.js
@@ -4,10 +4,11 @@ var xpos = 150;
 var ypos = 100;
 var count = 0;
 var x_dir = 1;
-var break_flag =0;
+var break_f_flag =0;//far from obstacle
+var break_l_flag =0;
+var break_r_flag =0;
 var ctx = document.getElementById("myCanvas").getContext('2d');
 var ang=0;
-var ang_tmp=0;
 var turn_flag=0;
 window.WebSocket = window.WebSocket || window.MozWebSocket;
 
@@ -34,10 +35,11 @@ function drawObj(ctx) {
     ctx.fillStyle = "orange";
 
 }
-function turn(ctx) {
+function turn(ang) {
 
+  ctx.clearRect(xpos-50, ypos-10, 160, 120);
   ctx.translate(xpos+50,ypos+20);
-  ctx.rotate(7);
+  ctx.rotate(ang);
   console.log("right: "+ang);
   xpos=-50;
   ypos=-20;
@@ -46,17 +48,72 @@ function turn(ctx) {
 function drawCar(ctx) {
 
   ctx.clearRect(xpos-50, ypos-10, 160, 120);
-  if (count==10) {
-
-      turn(ctx);
-
-    count=0;
+  if (count==70) {
+      update();
+      count=0;
   }  
   count+=1;
- 
+
   if(x_dir == 1){xpos += 5;}
     else if(x_dir>0){xpos -= 5;}
   ctx.fillRect(xpos, ypos, 100, 40);
   console.log(xpos);
+  var i =10;
+  var imgData = ctx.getImageData(xpos+100+(i*10), ypos+50, 1, 20);//front
+  for(i=9;i>2;i--){ //an obstacle at least 10 pixel width
+    for(j=0;j<imgData.data.length;j+=40){ //just check the red value magic num 450?
+      if(imgData.data[j]==255){
+        break_f_flag =i;
+        console.log("obstacle: "+i);
+        break;
+      }
+    }
+    imgData = ctx.getImageData(xpos+100+(i*10), ypos+50, 1, 20);
+  }
+  i =10;
+  var imgData = ctx.getImageData(xpos+50, ypos-20-(i*10), 20, 1);//left
+  for(i=9;i>2;i--){ 
+    for(j=0;j<imgData.data.length;j+=40){ 
+      if(imgData.data[j]==255){
+        break_l_flag =i;
+        console.log("obstacle: "+i);
+        break;
+      }
+    }
+    imgData = ctx.getImageData(xpos+50, ypos-20-(i*10), 20, 1);
+  }
+  i =10;
+  var imgData = ctx.getImageData(xpos+50, ypos+60+(i*10), 20, 1);//right
+  for(i=9;i>2;i--){ 
+    for(j=0;j<imgData.data.length;j+=40){ 
+      if(imgData.data[j]==255){
+        break_r_flag =i;
+        console.log("obstacle: "+i);
+        break;
+      }
+    }
+    imgData =ctx.getImageData(xpos+50, ypos+60+(i*10), 20, 1);
+  }
 }
+function update(){
+  // console.log("===========update==========");
+  cnnnt+=1;
+  // var connection = new WebSocket('ws://192.168.0.102:1337');
+  var connection = new WebSocket('ws://192.168.208.226:1337');
+  connection.onopen = function () {
+
+    var car_obj = {Name:'car',F:break_f_flag,L:break_l_flag,R:break_r_flag}
+    var json = JSON.stringify(car_obj);
+    connection.send(json);
+  };
+  connection.onmessage = function (evt){
+    var msgfromserver = evt.data;
+    connection.close();
+    var parse_msg =JSON.parse(msgfromserver)
+   if (parse_msg.Name==='motor') {
+      turn(parse_msg.ang);
+      }
+  }
+}
+
 init(); 
