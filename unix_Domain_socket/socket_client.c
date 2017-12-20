46e130c6d6bf73462c9eb35be229ee52fed7ba65
SarahYuHanCheng <bigbigtreeful@gmail.com>
2017-12-20 17:06:12 +0800

    ok to send + recv

--- unix_Domain_socket/socket_client.c
+++ unix_Domain_socket/socket_client.c
@@ -7,60 +7,55 @@
 #include "ipc.h"
 
 int main(int argc,char** argv){
-	int client_fd,len;
-	int i;
-	struct sockaddr_un addr;
-	struct ipc_msg *msg,*msg_send;
-	char msgbuf[MAX_BUF_LEN];
-	struct test_send_struct *st_msg;
-	msg=(struct ipc_msg*) msgbuf;
+    int client_fd,len;
+    int i;
+    struct sockaddr_un addr;
+    struct ipc_msg *msg,*msg_send;
+    char msgbuf[MAX_BUF_LEN],recv_buf[5000];
+    struct test_send_struct *st_msg;
+    msg=(struct ipc_msg*) msgbuf;
 
-	if((client_fd = socket(PF_UNIX,SOCK_STREAM,0))== -1){
-		perror("socket");
-		goto fail;
-	}
-	memset(&addr,0,sizeof(addr));
-	addr.sun_family = PF_UNIX;
-	strcpy(addr.sun_path,CLIENT_SOCK_FILE);
-	unlink(CLIENT_SOCK_FILE);
-	if(bind(client_fd,(struct sockaddr*)&addr,sizeof(addr))==-1)
-	{
-		perror("bind");
-		goto fail;
-	}
+    if((client_fd = socket(PF_UNIX,SOCK_STREAM,0))== -1){
+        perror("socket");
+        goto fail;
+    }
+    memset(&addr,0,sizeof(addr));
+    addr.sun_family = PF_UNIX;
+    strcpy(addr.sun_path,CLIENT_SOCK_FILE);
+    unlink(CLIENT_SOCK_FILE);
 
-	memset(&addr,0,sizeof(addr));
-	addr.sun_family = PF_UNIX;
-	strcpy(addr.sun_path,SERVER_SOCK_FILE);
-	if(connect(client_fd,(struct sockaddr*)&addr,sizeof(addr))==-1){
-		perror("55555");
-		goto fail;
-	}else{printf("connected\n");}
-	msg->type = 1;
+    if(bind(client_fd,(struct sockaddr*)&addr,sizeof(addr))==-1)
+    {
+        perror("bind");
+        goto fail;
+    }
+
+    memset(&addr,0,sizeof(addr));
+    addr.sun_family = PF_UNIX;
+    strcpy(addr.sun_path,SERVER_SOCK_FILE);
+    printf("%d\n",sizeof(addr));
+    if(connect(client_fd,(struct sockaddr*)&addr,sizeof(addr))==-1){
+        perror("55555");
+        goto fail;
+    }else{printf("connected\n");}
+    msg->type = 1;
 msg->sd_msg="www";
-	if(send(client_fd,msg->sd_msg,sizeof(msg),0)==-1){
-		perror("send");
-		goto fail;
-	}
-	if((len= recv(client_fd,msg,MAX_BUF_LEN,0))<0){
-		perror("recv");
-		goto fail;
-	}
-	printf("success recv %s\n",msg->data);
-	if (msg->len >= MAX_MSG_LEN)
-	{
-		st_msg = (struct test_send_struct*)msg -> data;
-		printf("st_msg is %s\n", st_msg->msg);
-		// msg->data[0] = 'A';
-		// memcpy(st_msg,msg->data,msg->len);
-
-		st_msg ="Abccc";
-		send(client_fd,st_msg,sizeof(st_msg),0);
-	}
-	fail:
-		if(client_fd>=0){
-			close(client_fd);
-		}
-		unlink(CLIENT_SOCK_FILE);
-		return 1;
+    if(send(client_fd,msg->sd_msg,sizeof(msg),0)==-1){
+        perror("send");
+        goto fail;
+    }
+    if((len= recv(client_fd,recv_buf,MAX_BUF_LEN,0))<0){
+        perror("recv");
+        goto fail;
+    }
+    printf("success recv %s\n",recv_buf);
+    st_msg ="Abccc";
+    send(client_fd,st_msg,sizeof(st_msg),0);
+  
+    fail:
+        if(client_fd>=0){
+            close(client_fd);
+        }
+        unlink(CLIENT_SOCK_FILE);
+        return 1;
 }
