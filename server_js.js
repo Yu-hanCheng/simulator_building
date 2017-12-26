40f082b81ad5f469d462702018fa84fb1e80fd49
SarahCheng <bigbigtreeful@gmail.com>
2017-12-26 16:02:56 +0800

    U_serverII with IPC and Websocket not multiserver yet

--- server_js.js
+++ server_js.js
@@ -1,165 +1,143 @@
-"use strict";
-// Optional. You will see this name in eg. 'ps' or 'top' command
 process.title = 'server_js';
-// Port where we'll run the websocket server
+var net = require('net');
+var fs = require('fs');
+var path = '/tmp/server_child.sock';
 var webSocketsServerPort = 1337;
-// websocket and http servers
 var webSocketServer = require('websocket').server;
 var http = require('http');
-// var unixSocket = require("unix-socket");
-// var tryList = [ "tmp/server.sock" ];
-// var path = unixSocket.availableStream(tryList);
-// //or 
-// unixSocket.availableStream(tryList, function(path) {
- 
-// });
-
-var net = require('net');
-var path = '/tmp/server_child.sock';
-var u_client = new net.Socket();
-u_client.connect(path);
 
 
-/**
- * Global variables
- */
-// latest 100 messages
-var history = [ ];
-// list of currently connected clients (users)
-var clients = [ ];
-var position = [200,300];
-/**
- * Helper function for escaping input strings
- */
-function htmlEntities(str) {
-  return String(str)
-      .replace(/&/g, '&amp;').replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+// *********** IPC with newcode ***************
+
+fs.unlink(path, function () {
+  var u_server= net.createServer(function(c) {
+
+    console.log('u_serverconnected');
+    c.on('data', (chunk) => { 
+      console.log(chunk.toString());
+     });
+    c.write('I\'m u_server\r\n'); 
+    c.write(u_json); //ultra_value, obj_pos
+
+    // c.on('end', function() {
+    //   console.log('u_serverdisconnected');
+    // });
+
+  });
+
+  u_server.listen(path, function() {
+    console.log('u_serverbound listen on %s', path);
+  });
+});
+
+//********** global var ************
+
+//to_browser
+var motor=
+{Name:'motor_dir',msg:{direct:'right',angle:90}}
+// direct=L_F, L_B,R_F, R_B,Forward,Backward,stop
+
+//from_browser
+var code
+={Name:'code',msg:'txt'}
+var ultra_value
+={Name:'ultra_value',msg:[1,1,1]}//F,L,R
+var des
+={Name:'des',msg:[0,0]}//x,y
+
+// to_child
+var  ultra_value
+={Name:'ultra_value',msg:[1,1,1]}
+
+// from_child
+var motor_c
+={Name:'motor_c',msg:[0,0,0,0],period:0}// L_F, L_B,R_F, R_B, time
+
+function Motor_to_Angle(){
+  //
+  if(motor_c.msg[0]=="HIGH"){
+    if (motor_c.msg[1]=="HIGH") {
+      if (motor_c.msg[2]=="HIGH" && motor_c.msg[3]=="LOW") {
+        motor.direct="L_F";motor.angle=45;
+      }else if(motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH") {
+        motor.direct="L_B";motor.angle=45;
+      } else{ //motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH"
+        motor.direct="stop";
+      }
+    }else{ //motor_c.msg[1]=="LOW"
+      if (motor_c.msg[2]=="HIGH" && motor_c.msg[3]=="LOW") {
+        motor.direct="Forward";
+      }else if(motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH") {
+        motor.direct="R_F";motor.angle=90;
+      } else{ //motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH"
+        motor.direct="R_F";motor.angle=45;
+      }
+    }
+  }else{ //motor_c.msg[0]=="LOW"
+      if (motor_c.msg[1]=="HIGH") { //L_B
+        if (motor_c.msg[2]=="HIGH" && motor_c.msg[3]=="LOW") {
+          motor.direct="L_F";motor.angle=90;
+        }else if(motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH") {
+          motor.direct="Backward";motor.angle=0;
+        } else{ //motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH"
+          motor.direct="R_B";motor.angle=45;
+        }
+      }else{ //motor_c.msg[1]=="LOW"
+        if (motor_c.msg[2]=="HIGH" && motor_c.msg[3]=="LOW") { //RF
+          motor.direct="L_F";motor.angle=45;
+        }else if(motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH") {
+          motor.direct="L_F";motor.angle=45;
+        } else{ //motor_c.msg[2]=="LOW" && motor_c.msg[3]=="HIGH"
+          motor.direct="stop";motor.angle=0;
+        }
+      }
+  }
+
 }
-var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
-/**
- * HTTP server
- */
+
+// *********** websocket with browser ************
+
 var server = http.createServer(function(request, response) {
-  // Not important for us. We're writing WebSocket server,
-  // not HTTP server
 });
 server.listen(webSocketsServerPort, function() {
   console.log((new Date()) + " Server is listening on port "
       + webSocketsServerPort);
 });
-/**
- * WebSocket server
- */
 var wsServer = new webSocketServer({
-  // WebSocket server is tied to a HTTP server. WebSocket
-  // request is just an enhanced HTTP request. For more info 
-  // http://tools.ietf.org/html/rfc6455#page-6
   httpServer: server
 });
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
 wsServer.on('request', function(request) {
-  // console.log((new Date()) + ' Connection from origin '
-      // + request.origin + '.');
-
   var connection = request.accept(null, request.origin); 
-  // we need to know client index to remove them on 'close' event
   var index = clients.push(connection) - 1;
   var userName = false;
   var userColor = false;
-var fs = require('fs');
-  // user sent some message
+  var fs = require('fs');
   connection.on('message', function(message) {
     if (message.type === 'utf8') { // accept only text
-    // first message sent by user is their name
       var msg = JSON.parse(message.utf8Data);
-      console.log(msg);
-      if (msg.Name === "car") {
-        userName = msg.Name;
-        userColor = colors.shift();
-          console.log(' User is known as: ' + userName
-                      + ' with ' + msg.F + ' x position');
-          fs.readFile('motor.txt', 'utf8', function (err,data) {
-            if (err) {
-              return console.log(err);
-            }
-
-          var motor = {Name:'motor',msg:data}
+      if (msg.Name === "ultra_value") {
+          ultra_value.msg=data;
           var msg_tocar = JSON.stringify(motor);
           clients[index].sendUTF(msg_tocar);
           console.log("in server readmotor");
-            console.log(msg_tocar); 
-          });
-          fs.writeFile("motor.txt", "x", function(err) {
-          if(err) { return console.log(err); }
-      });
-          var backtocar = {Name:'des',msg:position[0]}
-        
-        // var u_sen = {Name:'sensor_value',msg_to_u:msg.x_value}
-        // var u_json = JSON.stringify(u_sen);
-        // u_client.write(u_json);
-
-      } else if(msg.Name === "set"){ // log and broadcast the message
+          console.log(msg_tocar); 
+      } else if(msg.Name === "des"){ // log and broadcast the message
           position[0]=msg.br_msg;    
       }else if (msg.Name === "code") {
-        var u_code = {Name:'code',msg_to_u:msg.br_msg};
-        var u_code_json = JSON.stringify(u_code);
-        u_client.write(u_code_json);//send to child
-        // var fs = require('fs');
-        var the_code = JSON.stringify(msg.br_msg);
         fs.writeFile("newcode.c", msg.br_msg, function(err) {
           if(err) { return console.log("write file error"); }
       });
-      const child = require('child_process').exec;
-    var cmdString = 'pkill -f newcode; gcc newcode.c -o newcode; ./newcode';
+    const child = require('child_process').exec;
+    var cmdString = 'pkill -f newcode; gcc newcode.c -o newcode; nice -n 39 ./newcode ;';
     child(cmdString, (err, stdout, stderr) => {
       console.log(stdout);
     });
-
-      }else if(msg.Name === "obtc"){
-        //unix socket
-        var ultra_value_json = {df:msg.distance,dr:'100',dl:'20'}
-        var ultra_value = JSON.stringify(ultra_value_json);
-      fs.writeFile("ultra_value.txt", ultra_value, function(err) {
-          if(err) { return console.log(err); }
-      }); 
-      fs.writeFile("motor.txt", "right", function(err) {
-          if(err) { return console.log(err); }
-      });
-      // fs.writeFile("ultra_value.txt", ultra_value, function(err) {
-      //     if(err) { return console.log(err); }
-      // });  
-        // var u_obj = {Name:'u_obtc',msg_to_u:msg.x_value}
-        // var u_json = JSON.stringify(u_obj);
-        
       }
     }//if (message.type === 'utf8')
   });
   // user disconnected
   connection.on('close', function(connection) {
-    // if (userName !== false && userColor !== false) {
-    //   // console.log((new Date()) + " Peer "
-    //   //     + connection.remoteAddress + " disconnected.");
-    //   clients.splice(index, 1);
-    //   colors.push(userColor);
-    // }
+
     return console.log(connection);
   });
 });
-
-
-
-
-// another way 
-
-// var http = require('http');
-// var express = require('express');
-// var app = express();
-// var server = http.createServer(app);
-// app.get('/',function(request, response){ //我們要處理URL為 "/" 的HTTP GET請求
-//     response.end('你好！'); //作出回應
-// });
-// server.listen(8080,'127.0.0.1',function(){
-//     console.log('HTTP伺服器在 http://127.0.0.1:8080/ 上運行');
-// });
